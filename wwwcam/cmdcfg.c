826aa8db8f64756a3e1ee5c1de69cb8247ebd5d3
Mikhail Ryzhov <rymiser@gmail.com>
2015-04-21 22:53:01 +0300

    new interface of configuration files and cmdline parsers

--- wwwcam/cmdcfg.c
+++ wwwcam/cmdcfg.c
@@ -12,14 +12,12 @@ struct cmdcfg* cmdcfg_new(void)
 
 	cfg->names = calloc(32, sizeof(char*));
 	cfg->values = calloc(32, sizeof(char*));
-	cfg->funcs = calloc(32, sizeof(struct cmdfunc));
 	cfg->allocated = 32;
 	cfg->count = 0;
 
-	if (!cfg->names || !cfg->values || !cfg->funcs) {
+	if (!cfg->names || !cfg->values) {
 		free(cfg->names);
 		free(cfg->values);
-		free(cfg->funcs);
 		free(cfg);
 		return NULL;
 	}
@@ -37,10 +35,7 @@ void cmdcfg_free(struct cmdcfg *cfg)
 	for (i = 0; i < cfg->count; i++) {
 		free(cfg->names[i]);
 		free(cfg->values[i]);
-		if (cfg->funcs[i].freectx)
-			cfg->funcs[i].freectx(cfg->funcs[i].ctx);
 	}
-	free(cfg->funcs);
 	free(cfg->names);
 	free(cfg->values);
 	free(cfg);
@@ -75,13 +70,6 @@ static int cfg_alloc(struct cmdcfg *cfg)
 		return CMDCFG_ERR_MEMORY;
 	} else {
 		cfg->values = tmp;
-	}
-
-	tmp = realloc(cfg->funcs, cnt * sizeof(struct cmdfunc));
-	if (!tmp) {
-		return CMDCFG_ERR_MEMORY;
-	} else {
-		cfg->funcs = tmp;
 	}
 
 	cfg->allocated = cnt;
@@ -122,57 +110,9 @@ int cmdcfg_set(struct cmdcfg *cfg, const char *name, const char *value)
 		}
 
 		++cfg->count;
-	} else {
-		if (cfg->funcs[i].freectx) {
-			cfg->funcs[i].freectx(cfg->funcs[i].ctx);
-		}
 	}
 
 	cfg->values[i] = val;
-	cfg->funcs[i].freectx = NULL;
-	cfg->funcs[i].call = NULL;
-	cfg->funcs[i].ctx = NULL;
-
-	return 0;
-}
-
-int cmdcfg_set_func(struct cmdcfg *cfg,
-		const char *name,
-		int (*call)(void *ctx, int argc, char **argv, char **res),
-		void *ctx,
-		void (*freectx)(void *ctx))
-{
-	unsigned i;
-
-	if (!cfg || !name || !call) {
-		return CMDCFG_ERR_GENERAL;
-	}
-
-	for (i = 0; i < cfg->count; i++)
-		if (!strcmp(cfg->names[i], name))
-			break;
-
-	if (i == cfg->count) {
-		if (cfg->count >= cfg->allocated) {
-			if (cfg_alloc(cfg)) {
-				return CMDCFG_ERR_MEMORY;
-			}
-		}
-
-		cfg->names[i] = strdup(name);
-		if (!cfg->names[i]) {
-			return CMDCFG_ERR_MEMORY;
-		}
-
-		++cfg->count;
-	} else {
-		free(cfg->values[i]);
-	}
-
-	cfg->values[i] = NULL;
-	cfg->funcs[i].freectx = freectx;
-	cfg->funcs[i].call = call;
-	cfg->funcs[i].ctx = ctx;
 
 	return 0;
 }
@@ -194,13 +134,10 @@ int cmdcfg_del(struct cmdcfg *cfg, const char *name)
 
 	free(cfg->names[i]);
 	free(cfg->values[i]);
-	if (cfg->funcs[i].freectx)
-		cfg->funcs[i].freectx(cfg->funcs[i].ctx);
 
 	if (i != cfg->count - 1) {
 		cfg->names[i] = cfg->names[cfg->count - 1];
 		cfg->values[i] = cfg->values[cfg->count - 1];
-		memcpy(cfg->funcs + i, cfg->funcs + (cfg->count - 1), sizeof(struct cmdfunc));
 	}
 
 	--cfg->count;
@@ -208,50 +145,8 @@ int cmdcfg_del(struct cmdcfg *cfg, const char *name)
 	return 0;
 }
 
-/* Read one string from stream. This function will read:
- *   identifier
- *   $var
- *   ${var}
- *   [subcommand....]
- *   {string}
- *   Function returns end position of new word. If there are no new words function will return 0.
- *   On errors function returns -1
- *   Function sets *pos = start position on success
- */
-static ssize_t read_one(const char *str, ssize_t len, ssize_t *pos)
-{
-	ssize_t i;
-
-	i = *pos;
-	/* Searching for the word */
-
-
-
-}
-
 int cmdcfg_array_parse(const char *str, char **array, unsigned *len)
 {
 	return -1;
-}
-
-static int e_var();
-
-int cmdcfg_exec(struct cmdcfg *cfg, const char *str, ssize_t len)
-{
-	ssize_t l = len < 0? strlen(str): len;
-	ssize_t i = 0;
-
-	if (!cfg) {
-		return CMDCFG_ERR_GENERAL;
-	}
-
-	while (i < l && isspace(str[i])) {
-		++i;
-	}
-
-	if (str[i] == '#') { /* Skip comment */
-		while (i < l && str[i] != '\n')
-			++i;
-	}
 }
 
--- wwwcam/cmdcfg.h
+++ wwwcam/cmdcfg.h
@@ -1,6 +1,8 @@
 #ifndef CMD_CFG_H_INC
 #define CMD_CFG_H_INC
 
+/* Command line and configuration files parser in one file */
+
 #include <stdlib.h>
 #include <ctype.h>
 
@@ -12,33 +14,39 @@ extern "C" {
 #define CMDCFG_ERR_MEMORY  (-2)
 #define CMDCFG_ERR_SYNTAX  (-3)
 
-struct cmdfunc {
-	int (*call)(void *ctx, int argc, char **argv, char **res);
-	void *ctx;
-	void (*freectx)(void *ctx);
-};
-
 struct cmdcfg {
 	/* Variables: */
 	char **names;
 	char **values;
-	struct cmdfunc *funcs;
 	unsigned count;
 	unsigned allocated;
+};
+
+struct cmdcfg_option {
+	char opt;
+	const char *name;
+	const char *help;
+
+	int type;
+#define CMDCFG_TYPE_FLAG    1
+#define CMDCFG_TYPE_NUMBER  2
+#define CMDCFG_TYPE_STRING  3
+#define CMDCFG_TYPE_LIST    4
 };
 
 struct cmdcfg* cmdcfg_new(void);
 void cmdcfg_free(struct cmdcfg *cfg);
 
 int cmdcfg_set(struct cmdcfg *cfg, const char *name, const char *value);
-int cmdcfg_set_func(struct cmdcfg *cfg,
-		const char *name,
-		int (*call)(void *ctx, int argc, char **argv, char **res),
-		void *ctx,
-		void (*freectx)(void *ctx));
 int cmdcfg_del(struct cmdcfg *cfg, const char *name);
+
+const char* cmdcfg_get(struct cmdcfg *cfg, const char *name, const char *def);
+int cmdcfg_get_int(struct cmdcfg *cfg, const char *name, int def);
+int cmdcfg_get_list(struct cmdcfg *cfg, char **list, unsigned *len);
+
 int cmdcfg_array_parse(const char *str, char **array, unsigned *len);
-int cmdcfg_exec(struct cmdcfg *cfg, const char *str, ssize_t len);
+
+struct cmdcfg* cmdcfg_parse_options(int argc, const char *argv[], const char *config_file, struct cmdcfg_option *opts, unsigned opts_cnt);
 
 /* extern "C" { */
 #ifdef __cplusplus
